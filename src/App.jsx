b672ad57cb34caaf6ad1133f8e0498d2a13d4798
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-07-01 13:11:33 +0500

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElement,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
-  return <div className="text-5xl">App</div>;
+  const router = createBrowserRouter(
+    createRoutesFromElement(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
 
 const MainLayout = () => {
   return (
     <>
       <Navbar />
+      <Outlet />
     </>
   );
 };
